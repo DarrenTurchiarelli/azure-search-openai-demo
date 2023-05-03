51e7fec89f215cac5b34a9c74025c5fc75c2d390
Darren Turchiarelli <107463700+DarrenTurchiarelli@users.noreply.github.com>
2023-05-03 14:58:15 +1000

    Update sample questions

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "How does green power help the environment? ",
+        value: "How does green power help the environment? "
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "How do i read a water meter?", value: "How do i read a water meter?" },
+    { text: "What are some energy saving opportunities?", value: "What are some energy saving opportunities?" }
 ];
 
 interface Props {
